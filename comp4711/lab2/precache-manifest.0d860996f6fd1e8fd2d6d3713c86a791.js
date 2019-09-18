00009634beff1d67e60924c994570bdd472210de
Rongyi Chen <kenchenrong@gmail.com>
2019-09-18 14:13:39 -0700

    added lab2

---
+++ comp4711/lab2/precache-manifest.0d860996f6fd1e8fd2d6d3713c86a791.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7552f821ee3196d63a4db7e1203010e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1848a98bf55cf495c869",
+    "url": "/static/css/main.e423aa0d.chunk.css"
+  },
+  {
+    "revision": "5a7fcc0d892ca23dac80",
+    "url": "/static/js/2.39313b6b.chunk.js"
+  },
+  {
+    "revision": "1848a98bf55cf495c869",
+    "url": "/static/js/main.2122941a.chunk.js"
+  },
+  {
+    "revision": "543b2aac50024f383da3",
+    "url": "/static/js/runtime~main.9850ca97.js"
+  }
+]);
